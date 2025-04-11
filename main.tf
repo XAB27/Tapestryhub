07c97a185d4742be1bd38ef752c666c1810ebe7c
Xavier Baucicault <xavierbaucicault@macbookpro.attlocal.net>
2025-04-10 23:30:33 -0400

    Configured file

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "web" {
-  ami                    = "ami-0287a05f0ef0e9d9a"      #change ami id for different region
+resource "aws_instance" "NewStarbucks" {
+  ami                    = "ami-04f167a56786e4b09"      #change ami id for different region
   instance_type          = "t2.large"
-  key_name               = "Linux-VM-Key7"              #change key name as per your setup
-  vpc_security_group_ids = [aws_security_group.Jenkins-VM-SG.id]
+  key_name               = "Tapestrykey"              #change key name as per your setup
+  #vpc_security_group_ids = [aws_security_group.Jenkins-VM-SG.id]
   #user_data              = templatefile("./install.sh", {})
 
   tags = {
